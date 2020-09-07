5bc4c163f768f5ccee49a81d88382209184e274b
filipepaulino <felipepaulinobarbosa@gmail.com>
2020-09-07 10:59:28 -0300

    Atualizações da estrutura da aplicação.

--- src/pages/tela-inicial/tela-inicial.ts
+++ src/pages/tela-inicial/tela-inicial.ts
@@ -15,6 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TelaInicialPage {
 
+  n:number = 1;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
